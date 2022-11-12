80bef9dcdd28240dfc5d3f5cb8d44f4ce8d15422
Andres Agudelo <anagudelogu@gmail.com>
2022-11-12 08:19:12 -0500

    Add custom devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users,
              path: '',
-             path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' }
+             path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' },
+             controllers: { sessions: 'users/sessions', registrations: 'user/registrations' }
 
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
