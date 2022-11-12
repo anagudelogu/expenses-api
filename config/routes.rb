fbe1eb0e610ce9a7ac549ab0a2db2cd5abc1b22c
Andres Agudelo <anagudelogu@gmail.com>
2022-11-12 08:07:38 -0500

    Create User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users,
+             path: '',
+             path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' }
+
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    name { 'Jhon Doe' }
+    email { 'example@example.com' }
+    password { 'password' }
+  end
+end
